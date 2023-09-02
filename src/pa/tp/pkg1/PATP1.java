b5390fd207dd7860c31ecc50ac5ff91abe6bdd91
Sergio Rottchen 66832 <marta@DESKTOP-EQ1K9AK>
2023-09-02 10:10:59 -0300

    Carga inicial

---
+++ src/pa/tp/pkg1/PATP1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pa.tp.pkg1;
+
+import java.awt.BorderLayout;
+import javax.swing.text.AbstractDocument.Content;
+
+/**
+ *
+ * @author marta
+ */
+public class PATP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame Menu = new NewJFrame();
+        Menu.setTitle("PA 2 - Trabajo Practico Numero 1");
+        Menu.setLocationRelativeTo(null);
+        Menu.setVisible(true);
+        
+                   
+    }    
+}
